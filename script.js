ca6b97b53148d3eb944d568978828ccf9a2f4683
satyadeepgithub <92775324+satyadeepgithub@users.noreply.github.com>
2022-04-12 08:09:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+var myUnits = "%";
+const d = new Date();
+presentYear=d.getFullYear();
+function updateTextInput(val) {
+    document.getElementById('interestvalue').innerHTML=val +' '+ myUnits; 
+  }
+
+  function calculateInterest(){
+    var principal=document.getElementById('amount').value;
+    var rate=document.getElementById('interestRate').value;
+    var time=document.getElementById('years').value;
+    var result=(principal*rate*time)/100
+    var res= parseInt(result)
+    var princi=parseInt(principal)
+    var period=parseInt(time)
+    var total = res + princi
+    var finishingYear = presentYear + period
+    document.getElementById('enteredAmount').innerHTML = "If you deposit "+principal;
+    document.getElementById('enteredInterestRate').innerHTML = "at a rate of "+ rate+' '+myUnits;
+    document.getElementById('totalAmount').innerHTML = "You will receive an amount of "+total;
+    document.getElementById('finishingyear').innerHTML = "in the year "+ finishingYear;
     
-}
-        
+  }
