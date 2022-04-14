c1c6f4578e64d59dab1a6e04aeb05060ef1f7b8b
satyadeepgithub <92775324+satyadeepgithub@users.noreply.github.com>
2022-04-14 12:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
+
 var myUnits = "%";
 const d = new Date();
 presentYear=d.getFullYear();
 function updateTextInput(val) {
     document.getElementById('interestvalue').innerHTML=val +' '+ myUnits; 
   }
+   
+  function getfocus() {
+    document.getElementById("amount").focus();
+  }
 
   function calculateInterest(){
     var principal=document.getElementById('amount').value;
+    if (isNaN(principal) || principal <= 0) {
+      alert("Enter a Positive Number");
+      return getfocus();
+    } 
     var rate=document.getElementById('interestRate').value;
     var time=document.getElementById('years').value;
-    var result=(principal*rate*time)/100
-    var res= parseInt(result)
-    var princi=parseInt(principal)
-    var period=parseInt(time)
-    var total = res + princi
-    var finishingYear = presentYear + period
-    document.getElementById('enteredAmount').innerHTML = "If you deposit "+principal;
-    document.getElementById('enteredInterestRate').innerHTML = "at a rate of "+ rate+' '+myUnits;
-    document.getElementById('totalAmount').innerHTML = "You will receive an amount of "+total;
-    document.getElementById('finishingyear').innerHTML = "in the year "+ finishingYear;
-    
+    var result=(principal*rate*time)/100;
+    var princi=parseInt(principal);
+    var period=parseInt(time);
+    var total = result + princi;
+    var finishingYear = presentYear + period;
+    finalRate=rate+myUnits;
+    document.getElementById('enteredAmount').innerHTML = "If you deposit <span id='finalPrincipal'>"+principal+"</span>,";
+    document.getElementById('enteredInterestRate').innerHTML = "at a rate of <span id='finalRate'>"+ finalRate+"</span>."
+    document.getElementById('totalAmount').innerHTML = "You will receive an amount of <span id='total'>"+total+"</span>,";
+    document.getElementById('finishingyear').innerHTML = "in the year <span id='end'>"+ finishingYear;+"</span>"
+    return dynamicStyle();
   }
+  function dynamicStyle(){
+    document.getElementById('finalPrincipal').style.backgroundColor="yellow";
+    document.getElementById('finalRate').style.backgroundColor="yellow";
+    document.getElementById('total').style.backgroundColor="yellow";
+    document.getElementById('end').style.backgroundColor="yellow";
+   
+  }
+
+
+     
+  
+
+  
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+  
